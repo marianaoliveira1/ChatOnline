a540b693a2b2040b3c48a1a241f8b78d34ca48fa
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-11-03 19:03:02 -0300

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat/screens/welcome/welcome_screen.dart';
+import 'package:chat/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      themeMode: ThemeMode.system,
       home: WelcomeScreen(),
     );
   }

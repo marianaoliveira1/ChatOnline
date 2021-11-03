165112f7e360d7b537bc6765676128b425eaa665
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-11-03 18:30:48 -0300

    images and themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chat/screens/welcome/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(MyApp());
-
-  Firestore.instance.collection("col").document("doc").setData({"texto":"mariana"});
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+      home: WelcomeScreen(),
     );
   }
 }
